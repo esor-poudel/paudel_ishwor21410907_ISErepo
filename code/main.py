666cd7faab60f8e5cc9c77f1456803620b32c44f
esor <esor.poudel@gmail.com>
2024-05-15 16:52:46 +0800

    skeleton for the project is created

---
+++ code/main.py
@@ -0,0 +1,8 @@
+from generation import find_user_generation
+from numerical import *
+
+def make_analysation():
+    pass
+
+if __name__ == "__main__":
+    pass
---
+++ code/numerical.py
@@ -0,0 +1,8 @@
+def calculate_life_path():
+    pass
+
+def find_lucky_color():
+    pass
+
+def find_master_number():
+    pass
