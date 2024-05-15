d80f153e2b0cf8ebcca05bcf65294b367c4fd0e5
esor <esor.poudel@gmail.com>
2024-05-15 18:56:55 +0800

    calculate_life_path module is completed

--- code/main.py
+++ code/main.py
@@ -1,8 +1,11 @@
 from generation import find_user_generation
 from numerical import *
 
-def make_analysation():
-    pass
+def make_analysation(birth_day):
+    life_path = calculate_life_path(birth_day)
+    return life_path
 
 if __name__ == "__main__":
-    pass
+    date = "13/11/1987"
+    path = make_analysation(date)
+    print(path)
--- code/numerical.py
+++ code/numerical.py
@@ -1,8 +1,30 @@
-def calculate_life_path():
-    pass
+def check_if_mater_number(num):
+    if num in [11,22,33]:
+        return True
+    return False
+
+def single_digit_number(num):
+    if check_if_mater_number(num):
+        return num
+    while num > 9:
+        num = sum(int(digit) for digit in str(num))
+    return num        
+
+
+def calculate_life_path(birth_day):
+    day,month,year = map(int, birth_day.split('/'))
+    
+    day_count = single_digit_number(day)
+    month_count = single_digit_number(month)
+    year_count = single_digit_number(year)
+    # print(day,month,year)
+    # print(day_count,month_count,year_count)
+    count = day_count + month_count + year_count
+    return single_digit_number(count)
 
 def find_lucky_color():
     pass
 
 def find_master_number():
     pass
+
